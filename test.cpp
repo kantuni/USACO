2d705562a88484336c5b0158e2fb37bb39d2e6cb
kantuni <henrikh.kantuni@gmail.com>
2018-08-26 13:55:37 +0400

    Test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/*
+  ID: henrikh1
+  TASK: test
+  LANG: C++14
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ifstream fin("test.in");
+  ofstream fout("test.out");
+  int a, b;
+  fin >> a >> b;
+  fout << a + b << "\n";
+  return 0;
+}
