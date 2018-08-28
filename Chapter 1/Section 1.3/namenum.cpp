1dd4db2627c7259cc1a48fcb27970a2ce018aab0
kantuni <henrikh.kantuni@gmail.com>
2018-08-28 10:32:37 +0400

    Name That Number

---
+++ Chapter 1/Section 1.3/namenum.cpp
@@ -0,0 +1,54 @@
+/*
+ID: henrikh1
+TASK: namenum
+LANG: C++14
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ifstream fin("namenum.in");
+  ofstream fout("namenum.out");
+  string num;
+  fin >> num;
+  ifstream din("dict.txt");
+  vector<string> names;
+  string name;
+  while (getline(din, name)) {
+    names.push_back(name);
+  }
+  map<char, char> memo;
+  memo['A'] = memo['B'] = memo['C'] = '2';
+  memo['D'] = memo['E'] = memo['F'] = '3';
+  memo['G'] = memo['H'] = memo['I'] = '4';
+  memo['J'] = memo['K'] = memo['L'] = '5';
+  memo['M'] = memo['N'] = memo['O'] = '6';
+  memo['P'] = memo['R'] = memo['S'] = '7';
+  memo['T'] = memo['U'] = memo['V'] = '8';
+  memo['W'] = memo['X'] = memo['Y'] = '9';
+  vector<string> match;
+  for (string name: names) {
+    if (name.size() != num.size()) {
+      continue;
+    }
+    bool valid = true;
+    for (int i = 0; i < name.size(); i++) {
+      if (memo[name[i]] != num[i]) {
+        valid = false;
+        break;
+      }
+    }
+    if (valid) {
+      match.push_back(name);
+    }
+  }
+  if (match.size() == 0) {
+    fout << "NONE\n";
+  } else {
+    for (string m: match) {
+      fout << m << "\n";
+    }
+  }
+  return 0;
+}
