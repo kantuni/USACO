edf096ee4161caaa069f5123ac19aaf2c51f7e80
kantuni <henrikh.kantuni@gmail.com>
2018-08-30 23:45:54 +0400

    Barn Repair

---
+++ Chapter 1/Section 1.4/barn1.cpp
@@ -0,0 +1,42 @@
+/*
+ID: henrikh1
+TASK: barn1
+LANG: C++14
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ifstream fin("barn1.in");
+  ofstream fout("barn1.out");
+  int m, s, c;
+  fin >> m >> s >> c;
+  vector<int> stalls(c);
+  for (int i = 0; i < c; i++) {
+    fin >> stalls[i];  
+  }
+  sort(stalls.begin(), stalls.end());
+  vector<int> diff(c - 1);
+  multimap<int, int> memo;
+  for (int i = 0; i < c - 1; i++) {
+    diff[i] = stalls[i + 1] - stalls[i];
+    memo.insert({diff[i], i});
+  }
+  set<int> breaks;
+  for (auto it = memo.rbegin(); m > 1; it++, m--) {
+    int index = (*it).second;
+    breaks.insert(index);
+  }
+  int ans = 0;
+  int curr = stalls[0];
+  for (int i = 0; i < c; i++) {
+    if (breaks.count(i)) {
+      ans += stalls[i] - curr + 1;
+      curr = stalls[i + 1];
+    }
+  }
+  ans += stalls[c - 1] - curr + 1;
+  fout << ans << "\n";
+  return 0;
+}
