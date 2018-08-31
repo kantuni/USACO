7e1d41fd726f7898beba852b8f09094a6b53ca92
kantuni <henrikh.kantuni@gmail.com>
2018-08-31 07:36:50 +0400

    Update crypt1.cpp

--- Chapter 1/Section 1.4/crypt1.cpp
+++ Chapter 1/Section 1.4/crypt1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 set<int> used;
 
-bool crypta(int n, int len) {
+bool check(int n, int len) {
   string num = to_string(n);
   while (n > 0) {
     if (!used.count(n % 10)) {
@@ -31,21 +31,15 @@ int main() {
     used.insert(d[i]);
   }
   int ans = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
-        for (int l = 0; l < n; l++) {
-          for (int m = 0; m < n; m++) {
-            int x = d[i] * 100 + d[j] * 10 + d[k];
-            int y = 10 * d[l] + d[m];
-            int p1 = x * d[m];
-            int p2 = x * d[l];
-            int p = p2 * 10 + p1;
-            if (crypta(p1, 3) and crypta(p2, 3) and crypta(p, 4)) {
-              ans++;
-            }
-          }
-        }
+  for (int i = 100; i < 1000; i++) {
+    for (int j = 10; j < 100; j++) {
+      int x = i * (j % 10);
+      int y = i * (j / 10);
+      int p = y * 10 + x;
+      bool ok = check(i, 3) and check(j, 2) and check(x, 3) and
+                check(y, 3) and check(p, 4);
+      if (ok) {
+        ans++;
       }
     }
   }
