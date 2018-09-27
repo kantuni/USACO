bb593ac2af78ade447b3a2929a8d64f23def72fe
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-09-27 14:53:42 +0400

    Update sprime.cpp

--- Chapter 1/Section 1.6/sprime.cpp
+++ Chapter 1/Section 1.6/sprime.cpp
@@ -11,9 +11,13 @@ ifstream fin("sprime.in");
 ofstream fout("sprime.out");
 
 bool prime(int n) {
-  if (n < 2) return false;
+  if (n < 2) {
+    return false;
+  }
   for (int i = 2; i <= sqrt(1. * n); i++) {
-    if (n % i == 0) return false;
+    if (n % i == 0) {
+      return false;
+    }
   }
   return true;
 }
