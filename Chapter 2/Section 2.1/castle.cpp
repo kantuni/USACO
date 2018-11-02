9b2ab30d767cc7303cb13cd31f7829112a4aae64
kantuni <henrikh.kantuni@gmail.com>
2018-11-02 11:45:48 -0400

    Update castle.cpp

--- Chapter 2/Section 2.1/castle.cpp
+++ Chapter 2/Section 2.1/castle.cpp
@@ -1,3 +1,9 @@
+/*
+ID: henrikh1
+TASK: castle
+LANG: C++14
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,56 +15,100 @@ int dfs(int i, int j, int comp) {
     return 0;
   }
   color[i][j] = comp;
-  int size = 0;
   int cell = mp[i][j];
-  if (cell < 8) {
+  int size = 0;
+  if ((cell & 8) == 0) {
     size += dfs(i + 1, j, comp);
   }
-  cell = (cell < 8) ? cell : cell - 8;
-  if (cell < 4) {
+  if ((cell & 4) == 0) {
     size += dfs(i, j + 1, comp);
   }
-  cell = (cell < 4) ? cell : cell - 4;
-  if (cell < 2) {
+  if ((cell & 2) == 0) {
     size += dfs(i - 1, j, comp);
   }
-  cell = (cell < 2) ? cell : cell - 2;
-  if (cell < 1) {
+  if ((cell & 1) == 0) {
     size += dfs(i, j - 1, comp);
   }
-  cell = (cell < 1) ? cell : cell - 1;
   return size + 1;
 }
 
 int main() {
   ifstream fin("castle.in");
-  // ofstream fout("castle.out");
+  ofstream fout("castle.out");
   int n, m;
   fin >> m >> n;
   mp.assign(n, vector<int>(m));
-  color.assign(n, vector<int>(m));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       fin >> mp[i][j];
     }
   }
   int comp = 1;
-  int mx = -1;
+  int mr = -1;
+  color.assign(n, vector<int>(m));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       if (color[i][j] == 0) {
-        mx = max(mx, dfs(i, j, comp));
+        mr = max(mr, dfs(i, j, comp));
         comp++;
       }
     }
   }
+  fout << comp - 1 << "\n";
+  fout << mr << "\n";
+  int rx, ry, rs;
+  int nmr = -1;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      cout << color[i][j] << " ";
+      for (int k = 0; k < 4; k++) {
+        int s = pow(2, k);
+        if (i == 0 and s == 2) {
+          continue;
+        }
+        if (j == 0 and s == 1) {
+          continue;
+        }
+        if (i == n - 1 and s == 8) {
+          continue;
+        }
+        if (j == m - 1 and s == 4) {
+          continue;
+        }
+        if (mp[i][j] & s) {
+          mp[i][j] -= s;
+          comp = 1;
+          mr = -1;
+          color.assign(n, vector<int>(m));
+          for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+              if (color[r][c] == 0) {
+                mr = max(mr, dfs(r, c, comp));
+                comp++;
+              }
+            }
+          }
+          if (mr > nmr) {
+            rx = i;
+            ry = j;
+            rs = s;
+            nmr = mr;
+          } else if (mr == nmr) {
+            bool fw = j < ry;
+            bool fs = j == ry and i > rx;
+            bool nbe = j == ry and i == rx and rs > s;
+            if (fw or fs or nbe) {
+              rx = i;
+              ry = j;
+              rs = s;
+            }
+          }
+          mp[i][j] += s;
+        }
+      }
     }
-    cout << "\n";
   }
-  cout << comp - 1 << "\n";
-  cout << mx << "\n";
+  char side = (rs == 1) ? 'W' : (rs == 2) ? 'N' : (rs == 4) ? 'E' : 'S';
+  fout << nmr << "\n";
+  fout << rx + 1 << " " << ry + 1 << " " << side << "\n";
   return 0;
 }
