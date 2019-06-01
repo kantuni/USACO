afb85096fabc032ca8f9966e7605776a53f4725e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-01 18:49:15 -0400

    Update milk.cpp

--- Chapter 1/Section 1.4/milk.cpp
+++ Chapter 1/Section 1.4/milk.cpp
@@ -12,21 +12,20 @@ int main() {
   ofstream fout("milk.out");
   int n, m;
   fin >> n >> m;
-  if (n == 0 or m == 0) {
-    fout << "0\n";
-    return 0;
-  }
-  vector<pair<int, int>> farmers(m);
-  for (int i = 0; i < m; i++) {
-    fin >> farmers[i].first >> farmers[i].second;
-  }
-  sort(farmers.begin(), farmers.end());
   int ans = 0;
-  for (int i = 0; n > 0 and i < m; i++) {
-    int q = min(n, farmers[i].second);
-    ans += farmers[i].first * q;
-    n -= q;
+  if (n > 0 and m > 0) {
+    vector<pair<int, int>> farmers(m);
+    for (int i = 0; i < m; i++) {
+      fin >> farmers[i].first >> farmers[i].second;
+    }
+    sort(farmers.begin(), farmers.end());
+    int ans = 0;
+    for (int i = 0; n > 0 and i < m; i++) {
+      int q = min(n, farmers[i].second);
+      ans += farmers[i].first * q;
+      n -= q;
+    }
   }
-  fout << ans << "\n";
+  fout << ans << endl;
   return 0;
 }
