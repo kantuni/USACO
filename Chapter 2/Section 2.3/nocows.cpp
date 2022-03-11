b36f19db5d4a3ff0e616918b04ade73bf17574a2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-03-11 11:16:39 -0500

    Cow Pedigrees: Accepted

--- Chapter 2/Section 2.3/nocows.cpp
+++ Chapter 2/Section 2.3/nocows.cpp
@@ -4,34 +4,63 @@ TASK: nocows
 LANG: C++14
 */
 
-// WA
 #include <bits/stdc++.h>
-#define P 9901
 using namespace std;
 
-map<int, map<int, int>> memo;
+const int P = 9901;
+const int N = 200;
+const int K = 100;
+long long memo[N][K];
 
-int solve(int n, int k) {
-  if (n < 3 or k < 2) {
+long long solve(long long n, long long k);
+long long remember(long long n, long long k);
+
+long long min_n(long long k) {
+  return 2 * k - 1;
+}
+
+long long max_n(long long k) {
+  return (1LL << k) - 1;
+}
+
+long long solve(long long n, long long k) {
+  bool c1 = n % 2 == 0;
+  bool c2 = n < min_n(k);
+  if (c1 or c2) {
     return 0;
   }
-  if (memo[n].count(k) == 0) {
-    int c1 = solve(n - 2, k) / 2;
-    int c2 = solve(n - 2, k - 1) * (n - pow(2, k - 2) - 1);
-    memo[n][k] = c1 + c2;
+  if (n == max_n(k)) {
+    return 1;
   }
-  return memo[n][k] % P;
+  long long ans = 0;
+  for (long long i = 1; n - 2 * i > 0; i++) {
+    long long l = n - 2 * i;
+    long long r = n - 1 - l;
+    for (long long j = 1; j < k - 1; j++) {
+      long long p1 = remember(l, k - 1) * remember(r, j) % P;
+      long long p2 = remember(l, j) * remember(r, k - 1) % P;
+      ans = (ans + p1 + p2) % P;
+    }
+    long long p3 = remember(l, k - 1) * remember(r, k - 1) % P;
+    ans = (ans + p3) % P;
+  }
+  return ans;
+}
+
+long long remember(long long n, long long k) {
+  if (memo[n][k] == -1) {
+    memo[n][k] = solve(n, k) % P;
+  }
+  return memo[n][k];
 }
 
 int main() {
   ifstream fin("nocows.in");
   ofstream fout("nocows.out");
-  int n, k;
-  cin >> n >> k;
-  for (int i = 0; i <= k; i++) {
-    memo[(1 << i) - 1][i] = 1;
-  }
-  int ans = solve(n, k);
-  cout << ans << endl;
+  memset(memo, -1, sizeof memo);
+  long long n, k;
+  fin >> n >> k;
+  long long ans = solve(n, k);
+  fout << ans << endl;
   return 0;
 }
