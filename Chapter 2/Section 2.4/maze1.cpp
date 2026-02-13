46d743eb71a700366cd16b0fe70c638ac196f188
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-02-13 15:24:07 +0400

    Refactor maze1.cpp

--- Chapter 2/Section 2.4/maze1.cpp
+++ Chapter 2/Section 2.4/maze1.cpp
@@ -7,16 +7,37 @@ LANG: C++17
 #include <bits/stdc++.h>
 using namespace std;
 
+int w, h;
+vector<vector<char>> M;
 char dr[] = {0, 0, 1, -1};
 char dc[] = {1, -1, 0, 0};
+
+vector<vector<int>> bfs(int er, int ec) {
+  vector<vector<int>> D(2 * h + 1, vector<int>(2 * w + 1));
+  queue<tuple<int, int, int>> q;
+  q.push({er, ec, 1});
+  while (!q.empty()) {
+    auto [r, c, d] = q.front(); q.pop();
+    for (int i = 0; i < 4; i++) {
+      int nr = r + dr[i];
+      int nc = c + dc[i];
+      bool rok = nr >= 0 and nr < 2 * h + 1;
+      bool cok = nc >=0 and nc < 2 * w + 1;
+      if (rok and cok and M[nr][nc] == ' ' and D[nr][nc] == 0) {
+        q.push({nr, nc, d + 1});
+        D[nr][nc] = d + 1;
+      }
+    }
+  }
+  return D;
+}
 
 int main() {
   ifstream fin("maze1.in");
   ofstream fout("maze1.out");
-  int w, h;
   fin >> w >> h;
   fin.ignore();
-  char M[2 * h + 1][2 * w + 1];
+  M.assign(2 * h + 1, vector<char>(2 * w + 1));
   vector<pair<int, int>> exits;
   for (int r = 0; r < 2 * h + 1; r++) {
     string row;
@@ -30,47 +51,15 @@ int main() {
       }
     }
   }
-  // BFS for Exit #1
-  queue<tuple<int, int, int>> q1;
-  map<pair<int, int>, int> d1;
-  auto [re1, ce1] = exits[0];
-  q1.push({re1, ce1, 1});
-  d1[{re1, ce1}] = 1;
-  while (!q1.empty()) {
-    auto [r, c, d] = q1.front(); q1.pop();
-    for (int i = 0; i < 4; i++) {
-      int nr = r + dr[i];
-      int nc = c + dc[i];
-      bool rok = nr >= 0 and nr < 2 * h + 1;
-      bool cok = nc >=0 and nc < 2 * w + 1;
-      if (rok and cok and M[nr][nc] == ' ' and d1.count({nr, nc}) == 0) {
-        q1.push({nr, nc, d + 1});
-        d1[{nr, nc}] = d + 1;
-      }
-    }
-  }
-  // BFS for Exit #2
-  queue<tuple<int, int, int>> q2;
-  map<pair<int, int>, int> d2;
-  auto [re2, ce2] = exits[1];
-  q2.push({re2, ce2, 1});
-  d2[{re2, ce2}] = 1;
-  while (!q2.empty()) {
-    auto [r, c, d] = q2.front(); q2.pop();
-    for (int i = 0; i < 4; i++) {
-      int nr = r + dr[i];
-      int nc = c + dc[i];
-      bool rok = nr >= 0 and nr < 2 * h + 1;
-      bool cok = nc >=0 and nc < 2 * w + 1;
-      if (rok and cok and M[nr][nc] == ' ' and d2.count({nr, nc}) == 0) {
-        q2.push({nr, nc, d + 1});
-        d2[{nr, nc}] = d + 1;
-      }
-    }
-  }
+  auto [er1, ec1] = exits[0];
+  auto [er2, ec2] = exits[1];
+  auto D1 = bfs(er1, ec1);
+  auto D2 = bfs(er2, ec2);
   int ans = 0;
-  for (auto [p, d]: d1) {
-    ans = max(ans, min(d, d2[p]));
+  for (int r = 0; r < 2 * h + 1; r++) {
+    for (int c = 0; c < 2 * w + 1; c++) {
+      ans = max(ans, min(D1[r][c], D2[r][c]));
+    }
   }
   fout << ans / 2 << "\n";
   return 0;
