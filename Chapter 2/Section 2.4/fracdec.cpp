e964dab23ce68ee348bf51a26b28612bea139fcc
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-03-03 02:48:30 +0400

    Pair is enough here

--- Chapter 2/Section 2.4/fracdec.cpp
+++ Chapter 2/Section 2.4/fracdec.cpp
@@ -9,11 +9,15 @@ using namespace std;
 
 ifstream fin("fracdec.in");
 ofstream fout("fracdec.out");
-map<int, map<int, char>> g;
+map<int, pair<int, char>> g;
 map<int, int> color;
 int cs = -1, cnt = 0;
 
 void print(char c) {
+  // Where does this come from?
+  if (c == '\0') {
+    return;
+  }
   fout << c;
   cnt++;
   if (cnt % 76 == 0) {
@@ -24,15 +28,16 @@ void print(char c) {
 
 void dfs(int u) {
   color[u] = 1;
-  for (auto [v, num]: g[u]) {
-    if (color[v] == 0) {
-      if (v == cs) {
-        print('(');
-      }
-      print(num);
-      dfs(v);
-    } else {
-      print(num);
+  auto [v, q] = g[u];
+  if (color[v] == 0) {
+    if (v == cs) {
+      print('(');
+    }
+    print(q);
+    dfs(v);
+  } else {
+    print(q);
+    if (cs != -1) {
       print(')');
     }
   }
@@ -45,11 +50,12 @@ int main() {
   int nc = n - a * d;
   while (nc % d > 0) {
     int rem = nc * 10 % d;
-    if (g[nc].count(rem) > 0) {
+    if (g.count(nc) > 0) {
       cs = rem;
       break;
     }
-    g[nc][rem] = (nc * 10 / d) + '0';
+    char q = (nc * 10 / d) + '0';
+    g[nc] = {rem, q};
     nc = rem;
   }
   string sa = to_string(a);
@@ -62,7 +68,7 @@ int main() {
     print('0');
   } else if (cs == u) {
     print('(');
-    print(g[u][u]);
+    print(g[u].second);
     print(')');
   } else {
     dfs(u);
