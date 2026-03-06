c7cdff4688b2e1821d2b04dff3b1820ff81b4f99
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-03-06 17:50:57 +0400

    Agri-Net: Accepted

---
+++ Chapter 3/Section 3.1/agrinet.cpp
@@ -0,0 +1,46 @@
+/*
+ID: henrikh1
+TASK: agrinet
+LANG: C++17
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1e9;
+
+int main() {
+  ifstream fin("agrinet.in");
+  ofstream fout("agrinet.out");
+  int n;
+  fin >> n;
+  int g[n][n];
+  for (int u = 0; u < n; u++) {
+    for (int v = 0; v < n; v++) {
+      fin >> g[u][v];
+    }
+  }
+  priority_queue<pair<int, int>> pq;
+  vector<int> color(n, 0);
+  vector<int> dist(n, INF);
+  pq.push({0, 0});
+  dist[0] = 0;
+  int ans = 0, cnt = 0;
+  while (cnt < n) {
+    auto [cost, u] = pq.top(); pq.pop();
+    if (color[u] == 1) {
+      continue;
+    }
+    color[u] = 1;
+    ans += -cost;
+    cnt++;
+    for (int v = 0; v < n; v++) {
+      if (color[v] == 1 or g[u][v] == 0) {
+        continue;
+      }
+      pq.push({-g[u][v], v});
+    }
+  }
+  fout << ans << "\n";
+  return 0;
+}
